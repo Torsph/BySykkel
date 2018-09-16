6c1b97234a0911d4d678a57b69c105f7dd4b089d
Torsph <thomas@torp.io>
2018-09-16 22:50:29 +0200

    Remove token

--- BySykkel/BySykkel/AppDelegate.swift
+++ BySykkel/BySykkel/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let token = "***REMOVED***"
+        let token = ""
 
         let api = BySykkelAPI(token: token)
         let vm = BySykkelViewModel(api: api)
